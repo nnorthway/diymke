8e50c3898cf82fa1750192db439e617ccc6b84cf
Nate Northway <northwayn@gmail.com>
2016-07-12 13:16:33 -0500

    Error handling updated
    This fixes Issue #4

--- error-msg.php
+++ error-msg.php
@@ -1,16 +1,22 @@
 <?php include 'header.php'; ?>
-<main>
-  <div class='row'>
-    <div class='error-msg twelve'>
 <?php
   $status = $_SERVER['REDIRECT_STATUS'];
-  if ($status === 200) {echo "Document has been processed and sent to you";}
-  if ($status === 400) {echo "Bad HTTP request";}
-  if ($status === 401) {echo "Unauthorized - Invalid password";}
-  if ($status === 403) {echo "Forbidden";}
-  if ($status === 418) {echo "I don't know why this is here";}
-  if ($status === 500) {echo "Internal Server Error";}
-  ?>
+  $codes = array(
+    403 => '403: Forbidden',
+    404 => '404: Not Found',
+    405 => '405: Not Allowed',
+    408 => '408: Request Timeout',
+    500 => '500: Internal Service Error',
+    502 => '502: Bad Gateway',
+    504 => '504: Gateway Timeout'
+  );
+?>
+<div class='row'>
+<div class='gray twelve'>
+  <h1>Error <?php echo $codes[$status]; ?></h1>
+  <p>Our Team is working to fix this. We're sorry for the inconvenience. If you keep
+    experiencing this error, <a href='contact'>Please Contact Us</a></p>
 </div>
 </div>
+</main>
 <?php include 'footer.php'; ?>
